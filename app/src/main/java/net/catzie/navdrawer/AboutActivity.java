46b7151b893f2056f67c399f7b403f9a0249be1c
Catzie (MacMini) <i@catzie.net>
2016-06-27 10:48:57 +0800

    init

---
+++ app/src/main/java/net/catzie/navdrawer/AboutActivity.java
@@ -0,0 +1,15 @@
+package net.catzie.navdrawer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Catzie on 27/06/2016.
+ */
+public class AboutActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
